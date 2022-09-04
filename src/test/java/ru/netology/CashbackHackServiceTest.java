6517b8eab8dfae636bcbb73359bdf581f3022578
Lose88 <hotsting1888@gmail.com>
2022-09-04 22:32:10 +0300

    addTestsByJunit5

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -32,4 +32,29 @@ public class CashbackHackServiceTest {
         assertEquals(expected, actual);
     }
 
+    @org.junit.jupiter.api.Test
+    void showRemainIfAmountMoreThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 400;
+        int actual = service.remain(1600);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldfAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void showRemainIfAmountLowThan1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 700;
+        int actual = service.remain(300);
+        assertEquals(expected, actual);
+    }
 }
